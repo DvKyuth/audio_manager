3f013354531c5da26b64c5b7d4a47362fb1087cb
Hass Yuthanea <h.yuthanea@gmail.com>
2020-11-06 11:23:05 +0700

    remove more log

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/services.dart';
 import 'package:audio_manager/audio_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
@@ -25,18 +25,8 @@ class _MyAppState extends State<MyApp> {
   PlayMode playMode = AudioManager.instance.playMode;
 
   final list = [
-    {
-      "title": "Assets",
-      "desc": "assets playback",
-      "url": "assets/xv.mp3",
-      "coverUrl": "assets/ic_launcher.png"
-    },
-    {
-      "title": "network",
-      "desc": "network resouce playback",
-      "url": "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.m4a",
-      "coverUrl": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
-    }
+    {"title": "Assets", "desc": "assets playback", "url": "assets/xv.mp3", "coverUrl": "assets/ic_launcher.png"},
+    {"title": "network", "desc": "network resouce playback", "url": "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.m4a", "coverUrl": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"}
   ];
 
   @override
@@ -57,26 +47,25 @@ class _MyAppState extends State<MyApp> {
 
   void setupAudio() {
     List<AudioInfo> _list = [];
-    list.forEach((item) => _list.add(AudioInfo(item["url"],
-        title: item["title"], desc: item["desc"], coverUrl: item["coverUrl"])));
+    list.forEach((item) => _list.add(AudioInfo(item["url"], title: item["title"], desc: item["desc"], coverUrl: item["coverUrl"])));
 
     AudioManager.instance.audioList = _list;
     AudioManager.instance.intercepter = true;
     AudioManager.instance.play(auto: false);
 
     AudioManager.instance.onEvents((events, args) {
-      print("$events, $args");
+      //print("$events, $args");
       switch (events) {
         case AudioManagerEvents.start:
-          print(
-              "start load data callback, curIndex is ${AudioManager.instance.curIndex}");
+          //print(
+          //    "start load data callback, curIndex is ${AudioManager.instance.curIndex}");
           _position = AudioManager.instance.position;
           _duration = AudioManager.instance.duration;
           _slider = 0;
           setState(() {});
           break;
         case AudioManagerEvents.ready:
-          print("ready to play");
+          // print("ready to play");
           _error = null;
           _sliderVolume = AudioManager.instance.volume;
           _position = AudioManager.instance.position;
@@ -89,10 +78,10 @@ class _MyAppState extends State<MyApp> {
           _position = AudioManager.instance.position;
           _slider = _position.inMilliseconds / _duration.inMilliseconds;
           setState(() {});
-          print("seek event is completed. position is [$args]/ms");
+          // print("seek event is completed. position is [$args]/ms");
           break;
         case AudioManagerEvents.buffering:
-          print("buffering $args");
+          // print("buffering $args");
           break;
         case AudioManagerEvents.playstatus:
           isPlaying = AudioManager.instance.isPlaying;
@@ -127,13 +116,12 @@ class _MyAppState extends State<MyApp> {
     final audio = audioFile.buffer.asUint8List();
 
     final appDocDir = await getApplicationDocumentsDirectory();
-    print(appDocDir);
+    // print(appDocDir);
 
     final file = File("${appDocDir.path}/aLIEz.m4a");
     file.writeAsBytesSync(audio);
 
-    AudioInfo info = AudioInfo("file://${file.path}",
-        title: "file", desc: "local file", coverUrl: "assets/aLIEz.jpg");
+    AudioInfo info = AudioInfo("file://${file.path}", title: "file", desc: "local file", coverUrl: "assets/aLIEz.jpg");
 
     list.add(info.toJson());
     AudioManager.instance.audioList.add(info);
@@ -173,20 +161,15 @@ class _MyAppState extends State<MyApp> {
                 child: ListView.separated(
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text(list[index]["title"],
-                            style: TextStyle(fontSize: 18)),
+                        title: Text(list[index]["title"], style: TextStyle(fontSize: 18)),
                         subtitle: Text(list[index]["desc"]),
                         onTap: () => AudioManager.instance.play(index: index),
                       );
                     },
-                    separatorBuilder: (BuildContext context, int index) =>
-                        Divider(),
+                    separatorBuilder: (BuildContext context, int index) => Divider(),
                     itemCount: list.length),
               ),
-              Center(
-                  child: Text(_error != null
-                      ? _error
-                      : "${AudioManager.instance.info.title} lrc text: $_position")),
+              Center(child: Text(_error != null ? _error : "${AudioManager.instance.info.title} lrc text: $_position")),
               bottomPanel()
             ],
           ),
@@ -222,7 +205,7 @@ class _MyAppState extends State<MyApp> {
             IconButton(
               onPressed: () async {
                 bool playing = await AudioManager.instance.playOrPause();
-                print("await -- $playing");
+                // print("await -- $playing");
               },
               padding: const EdgeInsets.all(0.0),
               icon: Icon(
@@ -306,9 +289,7 @@ class _MyAppState extends State<MyApp> {
                   },
                   onChangeEnd: (value) {
                     if (_duration != null) {
-                      Duration msec = Duration(
-                          milliseconds:
-                              (_duration.inMilliseconds * value).round());
+                      Duration msec = Duration(milliseconds: (_duration.inMilliseconds * value).round());
                       AudioManager.instance.seekTo(msec);
                     }
                   },
@@ -327,9 +308,7 @@ class _MyAppState extends State<MyApp> {
     if (d == null) return "--:--";
     int minute = d.inMinutes;
     int second = (d.inSeconds > 60) ? (d.inSeconds % 60) : d.inSeconds;
-    String format = ((minute < 10) ? "0$minute" : "$minute") +
-        ":" +
-        ((second < 10) ? "0$second" : "$second");
+    String format = ((minute < 10) ? "0$minute" : "$minute") + ":" + ((second < 10) ? "0$second" : "$second");
     return format;
   }
 
